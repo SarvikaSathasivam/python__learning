64d1933665d48140f0d8e0846e20e8fb22aabe79
SarvikaSathasivam <sarvisatha12305@gmail.com>
2026-05-09 08:07:28 +0530

    day2 - variables

---
+++ day2.py
@@ -0,0 +1,12 @@
+#variables in python 
+name = "sarvika"
+age = 21
+college = "kct"
+year = 3
+goal = 100000
+
+print("my name is ",name)
+print("i am", age, " years old")
+print("i am doing my UG in", college)
+print("i am in my ", year, "now" )
+print("i have to earn", goal, "per month")
