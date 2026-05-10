65ec56cc7cec204bf48ab1b8423ba2bf24ac4ec0
SarvikaSathasivam <sarvisatha12305@gmail.com>
2026-05-10 11:37:28 +0530

    day3 - user input

---
+++ day3.py
@@ -0,0 +1,12 @@
+name = input ("may i know your good name?!")
+age = input ("how young aree you?!")
+city = input ("where are you from?")
+
+print("hello", name)
+print("i am", age ,"years young")
+print("i am from",city)
+
+num1=int(input("enter the first number:"))
+num2=int(input("enter the second number:"))
+sum = num1 + num2
+print("sum of ",num1,"and",num2, "is",sum)
