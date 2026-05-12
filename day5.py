833e64a914305b0b18cba6c9a70ba94ca583ef15
SarvikaSathasivam <sarvisatha12305@gmail.com>
2026-05-12 18:49:13 +0530

    day5 - while loop

---
+++ day5.py
@@ -0,0 +1,16 @@
+# While loop
+
+count = 1
+while count <= 5:
+    print("Count is:", count)
+    count = count + 1
+
+print("Loop finished!")
+
+# Multiplication table
+num = int(input("Enter a number for multiplication table: "))
+i = 1
+while i <= 10:
+    print(num, "×", i, "=", num * i)
+    i = i + 1
+    
