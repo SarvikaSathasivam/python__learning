29e22f74d1fc85652e79034322367de7b72a5fbe
SarvikaSathasivam <sarvisatha12305@gmail.com>
2026-05-12 18:52:57 +0530

    day5 - while_loop_updated

--- day5.py
+++ day5.py
@@ -13,4 +13,15 @@ i = 1
 while i <= 10:
     print(num, "×", i, "=", num * i)
     i = i + 1
-    
+
+
+
+
+# Sum of numbers using while loop
+num = int(input("Enter a number: "))
+i = 1
+total = 0
+while i <= num:
+    total = total + i
+    i = i + 1
+print("Sum of 1 to", num, "is:", total)
