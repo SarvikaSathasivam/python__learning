9ed43610273985313293a6b1989bf72ea36e2123
SarvikaSathasivam <sarvisatha12305@gmail.com>
2026-05-14 19:22:50 +0530

    day6 - armsrong & factorial

--- day6.py
+++ day6.py
@@ -8,3 +8,20 @@ for i in range(num, 0, -1):
     factorial *= i
 
 print("Output:", factorial)
+
+# Check Armstrong Number
+
+num = int(input("Enter a number: "))
+
+temp = num
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** 3
+    temp //= 10
+
+if sum == num:
+    print(num, "is an Armstrong number")
+else:
+    print(num, "is not an Armstrong number")
