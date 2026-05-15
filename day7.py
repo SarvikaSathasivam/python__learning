4959108f586f69f548d77a8efdcdde761c02f6d5
SarvikaSathasivam <sarvisatha12305@gmail.com>
2026-05-15 17:18:38 +0530

    day7.py palindrome

---
+++ day7.py
@@ -0,0 +1,16 @@
+# Python program to check palindrome number
+
+num = int(input("Enter a number: "))
+
+original = num
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+if original == reverse:
+    print("Palindrome number")
+else:
+    print("Not a palindrome number")
