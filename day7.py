5a86de24f2cbfa664083499ee4a9e14a7b86a5b1
SarvikaSathasivam <sarvisatha12305@gmail.com>
2026-05-15 17:21:39 +0530

    day7.py - palindrome,fibonacci

--- day7.py
+++ day7.py
@@ -14,3 +14,19 @@ if original == reverse:
     print("Palindrome number")
 else:
     print("Not a palindrome number")
+
+#fibonacci series
+# Python program to print Fibonacci series
+
+n = int(input("Enter number of terms: "))
+
+a = 0
+b = 1
+
+print("Fibonacci Series:")
+
+for i in range(n):
+    print(a, end=" ")
+    c = a + b
+    a = b
+    b = c
