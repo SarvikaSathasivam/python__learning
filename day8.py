ea4cace3fb8d784475018febbe649810d59d1fc3
SarvikaSathasivam <sarvisatha12305@gmail.com>
2026-05-16 18:29:32 +0530

    day8 - for loop

--- day8.py
+++ day8.py
@@ -11,3 +11,14 @@ for i in range(1, 6):
 num = int(input("Enter a number: "))
 for i in range(1, 11):
     print(num, "×", i, "=", num * i)
+
+# Problem 1 — Sum of first n numbers
+
+n = int(input("Enter a number: "))
+
+sum_n = n * (n + 1) // 2
+
+print("Sum =", sum_n)
+
+for i in range(2, 51, 2):
+    print(i)
